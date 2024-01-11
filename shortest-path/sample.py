26f51a4076dd7d27141eee578b8bfe1e00b2e1ea
Radovan Bast <bast@users.noreply.github.com>
2024-01-11 09:58:02 +0100

    more testing

--- shortest-path/sample.py
+++ shortest-path/sample.py
@@ -1,4 +1,5 @@
 import potpourri3d as pp3d
+import numpy as np
 
 V, F = pp3d.read_mesh("bunny_small.ply")
 
@@ -12,3 +13,22 @@ path_pts = path_solver.find_geodesic_path(v_start=14, v_end=22)
 
 # path_pts is a Vx3 numpy array of points forming the path
 print(path_pts)
+
+
+points = []
+for p in V:
+    x, y, z = tuple(p.tolist())
+    points.append((x, y, z))
+
+vertices = []
+for v in F:
+    i, j, k = tuple(v.tolist())
+    vertices.append((i, j, k))
+
+
+points = np.array(points)
+vertices = np.array(vertices)
+
+_solver = pp3d.EdgeFlipGeodesicSolver(points, vertices)
+result = _solver.find_geodesic_path(v_start=14, v_end=22)
+print(result)
