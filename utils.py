b8989d0a263b8c3567b615c19133bcb91cb34865
Radovan Bast <bast@users.noreply.github.com>
2024-01-12 18:37:00 +0100

    the same code was there twice

--- utils.py
+++ utils.py
@@ -60,54 +60,12 @@ def create_mesh(points, vertices):
     return mesh
 
 
-# keep only points and triangles where z > 0
 def filter_vertices(points, vertices):
     new_points = []
     indices = {}
     j = 0
     for i, (x, y, z) in enumerate(points):
-        if z > 0.0:
-            indices[i] = j
-            new_points.append((x, y, z))
-            j += 1
-
-    new_vertices = []
-    for i, j, k in vertices:
-        if i in indices and j in indices and k in indices:
-            new_vertices.append((indices[i], indices[j], indices[k]))
-
-    return new_points, new_vertices
-
-
-def remove_unreferenced_indices(points, vertices):
-    indices = set()
-    for i, j, k in vertices:
-        indices.add(i)
-        indices.add(j)
-        indices.add(k)
-
-    new_points = []
-    new_indices = {}
-    j = 0
-    for i in indices:
-        new_points.append(points[i])
-        new_indices[i] = j
-        j += 1
-
-    new_vertices = []
-    for i, j, k in vertices:
-        new_vertices.append((new_indices[i], new_indices[j], new_indices[k]))
-
-    return new_points, new_vertices
-
-
-# keep only points and triangles where z > 0
-def filter_vertices(points, vertices):
-    new_points = []
-    indices = {}
-    j = 0
-    for i, (x, y, z) in enumerate(points):
-        if z > 0.0:
+        if z > -5.0:
             indices[i] = j
             new_points.append((x, y, z))
             j += 1
