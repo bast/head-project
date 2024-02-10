8d2e4e4d2c1c68311296ececac4aecde9bbb4025
Radovan Bast <bast@users.noreply.github.com>
2024-02-10 15:08:47 +0100

    wipe the zip file after extracting

--- generate-data/build-ray.sh
+++ generate-data/build-ray.sh
@@ -7,6 +7,7 @@ ray_version="0998e93117accf5740c43af06fa194712dd6199f"
 wget https://github.com/bast/ray/archive/${ray_version}.zip
 
 unzip ${ray_version}.zip
+rm -f ${ray_version}.zip
 
 cd ray-${ray_version}
 
