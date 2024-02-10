1d3ea46c721be0f7a12c7964529d9fc1b3845d73
Radovan Bast <bast@users.noreply.github.com>
2024-02-10 15:28:47 +0100

    update ray build script

--- generate-data/build-ray.sh
+++ generate-data/build-ray.sh
@@ -2,7 +2,7 @@
 
 set -euf -o pipefail
 
-ray_version="0998e93117accf5740c43af06fa194712dd6199f"
+ray_version="aca2051359fc1f6c7f5064eebf13d8b703d06f71"
 
 wget https://github.com/bast/ray/archive/${ray_version}.zip
 
@@ -12,3 +12,6 @@ rm -f ${ray_version}.zip
 cd ray-${ray_version}
 
 cargo build --release
+
+cd ..
+mv ray-${ray_version} ray
