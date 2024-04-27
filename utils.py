79da0a08afdf3d3b6e1d36253b5cc274abda32d6
Radovan Bast <bast@users.noreply.github.com>
2024-04-27 21:49:42 +0200

    rm unused code (code moved to pre-processing step)

--- utils.py
+++ utils.py
@@ -20,61 +20,6 @@ def read_mesh(datafile):
     return points, vertices
 
 
-def filter_vertices(points, vertices):
-    new_points = []
-    indices = {}
-    j = 0
-    for i, (x, y, z) in enumerate(points):
-        indices[i] = j
-        new_points.append((x, y, z))
-        j += 1
-
-    new_vertices = []
-    for i, j, k in vertices:
-        if i in indices and j in indices and k in indices:
-            new_vertices.append((indices[i], indices[j], indices[k]))
-
-    return new_points, new_vertices
-
-
-def remove_unreferenced_indices(points, vertices):
-    indices = set()
-    for i, j, k in vertices:
-        indices.add(i)
-        indices.add(j)
-        indices.add(k)
-
-    new_points = []
-    new_indices = {}
-    j = 0
-    for i in indices:
-        new_points.append(points[i])
-        new_indices[i] = j
-        j += 1
-
-    new_vertices = []
-    for i, j, k in vertices:
-        new_vertices.append((new_indices[i], new_indices[j], new_indices[k]))
-
-    return new_points, new_vertices
-
-
-def read_ply(file_name):
-    V, F = pp3d.read_mesh(file_name)
-
-    vertices = []
-    for t in V:
-        x, y, z = tuple(t.tolist())
-        vertices.append((x, y, z))
-
-    faces = []
-    for t in F:
-        i, j, k = tuple(t.tolist())
-        faces.append((i, j, k))
-
-    return vertices, faces
-
-
 def distance_squared(p1, p2) -> float:
     dx = p1[0] - p2[0]
     dy = p1[1] - p2[1]
