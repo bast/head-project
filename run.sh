08dc1483744568ebace13ebe1ac57e76ec50c9b2
Radovan Bast <bast@users.noreply.github.com>
2024-04-27 21:45:39 +0200

    update run script

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # if venv.sif does not exist
 if [ ! -f venv.sif ]; then
-    singularity pull https://github.com/bast/singularity-venv/releases/download/0.3.0/venv.sif
+    singularity pull https://github.com/bast/apptainer-venv/releases/download/0.4.0/venv.sif
 fi
 
 ./venv.sif python app.py
