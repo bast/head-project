e3793bcf590a2c580b770c05d28f688178150396
Radovan Bast <bast@users.noreply.github.com>
2024-04-28 16:06:31 +0200

    formatting

--- geodesic.py
+++ geodesic.py
@@ -5,7 +5,6 @@ import numpy as np
 def create_solver(vertices, faces):
     vertices = np.array(vertices)
     faces = np.array(faces)
-
     return pp3d.EdgeFlipGeodesicSolver(vertices, faces)
 
 
@@ -14,7 +13,6 @@ def find_path(solver, v_start, v_end):
     for p in solver.find_geodesic_path(v_start, v_end):
         x, y, z = tuple(p.tolist())
         points.append((x, y, z))
-
     return _path_distance(points), points
 
 
